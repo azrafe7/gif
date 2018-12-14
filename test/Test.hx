734433a2c6ac8fb61e6872bb959ecc780bd9d82c
azrafe7 <azrafe7@gmail.com>
2018-12-14 14:24:57 +0100

    test: use createGradient() to make frames

--- test/Test.hx
+++ test/Test.hx
@@ -1,21 +1,24 @@
 
 import gif.GifEncoder;
+import Color;
 
 class Test {
 
     static var width = 32;
     static var height = 32;
-    static var delay = 1;
-    static var numFrames = 5;
+    static var delay = .25;
+    static var numFrames = 8;
+
+    static var gradient:Array<Int> = Color.createGradient([0xFF0000, 0xFFFF00, 0xFF0000], [128, 128]);
 
     static function main() {
 
         trace("creating test.gif (" + numFrames + " frames) ...");
-        trace("frames' size " + width + "x" + height + " ...");
+        trace("frames size " + width + "x" + height + " ...");
 
         var output = new haxe.io.BytesOutput();
         var palette_analyzer = GifPaletteAnalyzer.AUTO;
-        var encoder = new gif.GifEncoder(width, height, 1, GifRepeat.Infinite, palette_analyzer);
+        var encoder = new gif.GifEncoder(width, height, 0, GifRepeat.Infinite, palette_analyzer);
         var palette_analyzer_enum = palette_analyzer.match(GifPaletteAnalyzer.AUTO) ? " (" + @:privateAccess encoder.palette_analyzer_enum + ")" : "";
 
         trace("using palette analyzer " + palette_analyzer + palette_analyzer_enum + " ...");
@@ -40,7 +43,7 @@ class Test {
         js.Browser.document.body.appendChild(imageElement);
         imageElement.src = 'data:image/gif;base64,' + haxe.crypto.Base64.encode(bytes);
     #else
-        throw 'Unsupported platform!';
+        throw 'Unsupported platform for this test!';
     #end
 
         trace("done.");
@@ -50,22 +53,25 @@ class Test {
     static var count = 0;
     static function make_frame() {
 
-        var val   = 255;
-        var red   = count % 3 == 0 ? val : 0; // Std.random(255);
-        var green = count % 3 == 1 ? val : 0; // Std.random(255);
-        var blue  = count % 3 == 2 ? val : 0; // Std.random(255);
-        count++;
-
-        //var red   = Std.random(255);
-        //var green = Std.random(255);
-        //var blue  = Std.random(255);
+        var red   = 0;
+        var green = 0;
+        var blue  = 0;
 
         var pixels = new haxe.io.UInt8Array(width * height * 3);
+        var gradientLength = gradient.length;
         for(i in 0 ... width * height) {
+            var idx = Std.int(i * gradientLength / (height * width));
+            red   = (gradient[idx] >> 16) & 0xFF;
+            green = (gradient[idx] >> 8) & 0xFF;
+            blue  = (gradient[idx]) & 0xFF;
             pixels[i * 3 + 0] = red;
             pixels[i * 3 + 1] = green;
             pixels[i * 3 + 2] = blue;
         }
+
+        count++;
+        var head = gradient.splice(0, Std.int(gradientLength / numFrames));
+        gradient = gradient.concat(head);
 
         var frame: GifFrame = {
             delay: delay,
